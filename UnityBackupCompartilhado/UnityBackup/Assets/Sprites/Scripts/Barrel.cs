26c590c76155dd756ea3141acffe250086b0333a
CapivaraRockeira <rkmorizono@gmail.com>
2021-08-17 23:10:11 -0300

    Bug Fix Barrel Respawn 17.08.21 23:10

--- UnityBackupCompartilhado/UnityBackup/Assets/Sprites/Scripts/Barrel.cs
+++ UnityBackupCompartilhado/UnityBackup/Assets/Sprites/Scripts/Barrel.cs
@@ -2,24 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Barrel : MonoBehaviour {
+public class Barrel : MonoBehaviour
+{
     public int lives;
     public ParticleSystem smoke;
     public ParticleSystem explosion;
     Renderer rend;
     GameObject Respawn;
+    BoxCollider2D box2d;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rend = GetComponent<Renderer>();
         Respawn = GameObject.Find("BarrelSpawner");
+        if (!GameObject.Find("Test Explosion Prop")) Destroy(gameObject);
         rend.enabled = true;
         lives = 3;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lives < 1)
+        {
+            if (!explosion.IsAlive())
+            {
+                if (GameObject.Find("Test Explosion Prop"))
+                {
+                    Instantiate(gameObject, Respawn.transform.position, Quaternion.identity);
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
 
     private void OnParticleCollision(GameObject other)
     {
@@ -28,16 +43,10 @@ public class Barrel : MonoBehaviour {
         {
             smoke.Play();
         }
-
-            if (lives < 1)
+        if (lives < 1)
         {
             explosion.Play();
             rend.enabled = false;
-            if (GameObject.Find("Test Explosion Prop"))
-            {
-                Instantiate(gameObject, Respawn.transform.position, Quaternion.identity);
-            }
-            Destroy(gameObject,1f);
         }
     }
 }
