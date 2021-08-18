84399994f392a5147125a845e7f2d7d0ee39fa8d
CapivaraRockeira <rkmorizono@gmail.com>
2021-08-18 09:51:15 -0300

    Criação Cenário 1 18.08.21 9:50

--- UnityBackupCompartilhado/UnityBackup/Assets/Sprites/Scripts/Barrel.cs
+++ UnityBackupCompartilhado/UnityBackup/Assets/Sprites/Scripts/Barrel.cs
@@ -9,15 +9,14 @@ public class Barrel : MonoBehaviour
     public ParticleSystem explosion;
     Renderer rend;
     GameObject Respawn;
-    BoxCollider2D box2d;
     // Use this for initialization
     void Start()
     {
         rend = GetComponent<Renderer>();
         Respawn = GameObject.Find("BarrelSpawner");
         if (!GameObject.Find("Test Explosion Prop")) Destroy(gameObject);
-        rend.enabled = true;
-        lives = 3;
+        if(rend.enabled==false)rend.enabled = true;
+        if(lives<=0)lives = 3;
     }
 
     // Update is called once per frame
